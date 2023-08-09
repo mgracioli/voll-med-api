cc913638d7c7025b0f808f3852fb5f3b4b99dabc
mgracioli <michel-rg@hotmail.com>
2023-08-09 20:05:20 -0300

    Criação de migrations para a criação da tabela medicos e posterior adicao do campo "telefone"

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,9 +1,18 @@
 package med.voll.api.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
+        @NotBlank
         String logradouro,
-        String bairro, String cep,
+        @NotBlank
+        String bairro,
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotBlank
         String cidade,
+        @NotBlank
         String uf,
         String complemento,
         String numero
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -16,22 +16,19 @@ import med.voll.api.endereco.Endereco;
 public class Medico {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
-
     private String email;
-
+    private String telefone;
     private String crm;
-
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
-
     @Embedded   //apesar da classe Endereco estar separada da de Medico, ambas fazem parte da mesma tabela no banco de dados
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico medico) {
         this.nome = medico.nome();
         this.email = medico.email();
+        this.telefone = medico.telefone();
         this.crm = medico.crm();
         this.especialidade= medico.especialidade();
         this.endereco = new Endereco(medico.endereco());
