b8061446d14543ae444e9c12855d4d443010ab41
mgracioli <michel-rg@hotmail.com>
2023-08-14 21:18:31 -0300

    Implementação de configurações básicas do Spring security e criação da tabela de usuário para armazenar dados de login e senha

---
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurarions.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurarions {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build(); //desabilita a segurança contra csrf (como vamos usar tokens, o token já previne contra ataques csrf) e desabilita o processo de autenticação padrão que o spring security habilita (o formulario de login e senha que ele cria), STATELESS pq essa é uma API rest, STATEFUL seria se fosse uma aplicação Web que precisa guardar a sessão do usuário q está acessando para não precisar usar tokens
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
